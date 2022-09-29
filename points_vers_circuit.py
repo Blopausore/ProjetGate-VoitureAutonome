4e82232a5dfa2b37e192054a4e2772444767ab1e
Blopausore <you@example.com>
2022-09-29 15:13:36 +0200

    Premier script ! Debut du code pour passer des points au format circuit, je me faisais chier donc j'ai ecris ca

---
+++ points_vers_circuit.py
@@ -0,0 +1,35 @@
+#Entree : listes de points [(x,y)] 
+import numpy as np
+
+a=np.array([2.,3.])
+b=np.array([5.,4.])
+c=np.array([5.,1.])
+
+def N(v):
+    return sum([c for c in v])
+
+def virage(a,b,c,r): 
+    """
+    ENTREE :
+        a,b,c : numpy array
+            Trois poins qui composent un virage
+        r : int
+            Rayon du cercle
+        
+    SORTIE :
+        alpha : int
+            Angle effectuer durant le virage
+        h : int
+            Distance avant b a laquelle le virage debute
+    """
+    v1,v2=b-a,c-b
+    cos_alpha=np.dot(v1,v2)/(N(v1)*N(v2))
+    alpha=np.arccos(cos_alpha)
+    h = r*np.tan(alpha/2)
+    return alpha,h
+
+print(virage(a,b,c,1))
+
+
+
+
