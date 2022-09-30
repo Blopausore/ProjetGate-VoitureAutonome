46fe234d54311961532f8054b4af9211a19c6fed
Blopausore <you@example.com>
2022-09-30 15:39:14 +0200

    Enfin je decuve

--- pointcirucit.py
+++ pointcirucit.py
@@ -4,20 +4,21 @@
 import numpy as np
 
 
+
+
 a=np.array([2.,3.])
 b=np.array([5.,4.])
 c=np.array([5.,1.])
-d=np.array([1.,1.5])
-e=np.array([1.,1.])
+d=np.array([1.,1.])
 
 
-points=[[a,1],[b,0.5],[c,1.5],[d,1],[e,0.5]]
+points=[[a,0.5],[b,0.2],[c,0.5],[d,0.1]]
 
 
-##
-#%%
+
 def N(v):
-    return np.sqrt(sum([c**2 for c in v])) 
+    return np.sqrt(sum([c**2 for c in v]))
+
 
 def rad_to_deg(r):
     return r*180/np.pi
@@ -46,39 +47,32 @@ def virage(a,b,c,r):
 alpha,h=virage(a,b,c,1)
 print(h,rad_to_deg(alpha))
 
-def init_alpha(a,b):#Retourne l'angle initial de la course
-    v=b-a
-    return np.arccos(v[0]/N(v))
-
-
 def points_vers_circuit(points:list):
     if len(points) < 3 :
         print("Erreur : nombre de point insuffisant")
         exit()
-    alpha0=init_alpha(points[0][0],points[1][0])
-    point0=points[0]
-    point1=points[1]
-    circuit=[alpha0]
-    for _ in range(len(points)):  
+    alpha0=init_alpha(points[-1][0],points[-2][0])
+    point0=points[-1]
+    point1=points[-2]
+    circuit=[]
+    for i in range(len(points)):  
+        
         if len(points) == 2:
-            a,b,c=[c for c,_ in points]+[point0[0]]
+            a,b,c=points[1][0],points[0][0],point0[0]
             r=points[-2][1] 
-            print(a,b,c,r)
         elif len(points)==1:
             a,b,c=points[-1][0],point0[0],point1[0]
             r=point0[1]
-            print(a,b,c,r)
         else :
-            a,b,c=[c for c,_ in points[-3:]]
+            c,b,a=[c for c,_ in points[-3:]]
             r=points[-2][1]
         alpha,h=virage(a,b,c,r)
-        circuit.append(max(N(b-a)-h,0))
+        circuit.append(N(b-a)-h)
         circuit.append((r,alpha))
         points.pop()
-
-
-    
-
+        print("Tour",i)
+        print("\t",circuit[-1])
+        print("\t",a,b,c,r)
     return circuit
 
 
